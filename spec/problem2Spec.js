79a7d5f16cc89c7e8ffe7bf3ed53bc2ac92d5441
Anthony Lamorte <lamorte@gmail.com>
2015-01-26 20:33:16 -0500

    first crack at solution to problem 2

---
+++ spec/problem2Spec.js
@@ -0,0 +1,5 @@
+describe("Problem 2 solution", function() {
+  it("sums the even fibonacci numbers less than a given number", function() {
+    expect(sumEvenFibs(100)).toEqual(44);
+  });
+});
---
+++ src/problem2.js
@@ -0,0 +1,39 @@
+/*
+  Project Euler Problem 2
+  Even Fibonacci numbers
+*/
+
+function sumEvenFibs(limit) {
+  return getEvenFibs(limit).reduce(function(a, b) { return a + b; });
+}
+
+function getEvenFibs(limit) {
+  var evenFibs = [];
+  var fibs = getFibs(limit);
+
+  for (var i = 1; i < fibs.length; i++) {
+    if (fibs[i] % 2 === 0){
+      evenFibs.push(fibs[i]);
+    }
+  }
+
+  return evenFibs;
+}
+
+function getFibs(limit) {
+  var fibs = [1, 1];
+  var i = fibs.length;
+  var currentFib = 0;
+
+  do {
+    currentFib = fibs[i - 1] + fibs[i - 2];
+
+    if (currentFib < limit) {
+      fibs.push(currentFib);
+    }
+
+    i++;
+  } while (currentFib < limit);
+
+  return fibs;
+}
