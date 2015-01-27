4d5dc4e3ca05b5eeaca9bb351bdbf77b0fac6c3e
Anthony Lamorte <lamorte@gmail.com>
2015-01-26 20:30:12 -0500

    removing tests that test implementation details

--- spec/problem1Spec.js
+++ spec/problem1Spec.js
@@ -1,9 +1,5 @@
 describe("Problem 1 solution", function() {
-  it("returns array of multiples of 3 and 5", function() {
-    expect(getMultiples(10)).toEqual([3, 5, 6, 9]);
-  });
-
-  it("returns 23 for multiples of 3 and 5, less than 10", function() {
+  it("sums the multiples of 3 and 5 less than a given number", function() {
     expect(sumMultiplesOfThreeAndFive(10)).toEqual(23);
   });
 });
