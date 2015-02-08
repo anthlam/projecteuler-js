11c2fcc238e43198abb1de3316e642b73738137a
Anthony Lamorte <lamorte@gmail.com>
2015-02-08 14:58:37 -0500

    fixes #1 - combines getEvenFibs and getFibs functions

--- src/problem2.js
+++ src/problem2.js
@@ -8,30 +8,23 @@ function sumEvenFibs(limit) {
 }
 
 function getEvenFibs(limit) {
-  var evenFibs = [];
-  var fibs = getFibs(limit);
-
-  for (var i = 1, x = fibs.length; i < x; i++) {
-    if (fibs[i] % 2 === 0){
-      evenFibs.push(fibs[i]);
-    }
-  }
-
-  return evenFibs;
-}
-
-function getFibs(limit) {
-  var fibs = [1, 1];
-  var i = fibs.length;
-  var currentFib = fibs[i - 1] + fibs[i - 2];
+  var fibs = [1, 1],
+      evenFibs = [],
+      i = fibs.length,
+      currentFib = fibs[i - 1] + fibs[i - 2];
 
   while (currentFib < limit) {
 
     fibs.push(currentFib);
+
+    if (currentFib % 2 === 0) {
+      evenFibs.push(currentFib);
+    }
+
     i = fibs.length;
     currentFib = fibs[i - 1] + fibs[i - 2];
 
   }
 
-  return fibs;
+  return evenFibs;
 }
